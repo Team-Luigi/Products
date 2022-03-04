44f280e36803275384dd837d680b720bf02bb93a
Lorene Chew <lorene.chew@gmail.com>
2022-03-03 20:41:01 -0800

    server setup, db connected

---
+++ Server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const controller = require('./Controller');
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/api/products/:id', controller.getProduct);
+app.get('/api/products/:id/styles', controller.getStyles);
+
+
+
+app.listen(3000);
