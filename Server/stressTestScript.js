5fcd2f42b5d9cfa5dba1c216eec513ca7d60ebfc
Lorene Chew <lorene.chew@gmail.com>
2022-03-08 15:25:18 -0800

    added new relic, updated stressTest using http.batch to test multiple endpoints

--- Server/stressTestScript.js
+++ Server/stressTestScript.js
@@ -6,9 +6,16 @@ export const options = {
   duration: '15s',
 };
 
-const url = 'http://localhost:3000/api/products/1/styles'
+
+const id = Math.floor(Math.random() * 1000011);
+const urlProduct = `http://localhost:3000/api/products/${id}`
+const urlStyles = `http://localhost:3000/api/products/${id}/styles`
 
 export default function () {
-  http.get(url);
-  sleep(1);
+  // http.get(url);
+  // sleep(1);
+  const responses = http.batch([
+    ['GET', urlProduct],
+    ['GET', urlStyles]
+  ]);
 }
